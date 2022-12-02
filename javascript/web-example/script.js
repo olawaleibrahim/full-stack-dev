a3d82ac83202a202969acada90f5111f1a447f26
Olawale Ibrahim <ibrahim.olawale@earthanalytics.no>
2022-12-02 16:11:12 +0100

    loops and better sign in

--- javascript/web-example/script.js
+++ javascript/web-example/script.js
@@ -2,6 +2,14 @@ var database = [
     {
         username: "olawale",
         password: "supersecret"
+    },
+    {
+        username: "sally",
+        password: "123"
+    },
+    {
+        username: "ingrid",
+        password: "777"
     }
 ];
 
@@ -13,19 +21,32 @@ var newsFeed = [
     {
         username: "Sally",
         timeline: "Javascript is soo cool"
+    },
+    {
+        username: "Mitch",
+        timeline: "Javascript is preeettyyy cool"
     }
 ];
 
 var userNamePrompt = prompt("What is your username?");
 var passwordPrompt = prompt("what is your password?");
 
+function isUserValid(username, password) {
+    for (var i =0; i<database.length; i++) {
+        if (database[i].username === username && 
+            database[i].password === password) {
+                return true;
+            }
+    }
+    return false;
+}
+
 function signIn(user, pass) {
-    if (user == database[0].username 
-        && pass == database[0].password) {
-            console.log(newsFeed)
-        } else {
-            alert("Sorry, wrong username and password!");
-        }
+    if (isUserValid(user, pass)) {
+        console.log(newsFeed);
+    } else {
+        alert("Sorry, wrong username and password!");
+    }
 }
 
 signIn(userNamePrompt, passwordPrompt);
---
+++ javascript/web-example/script1.js
@@ -0,0 +1,47 @@
+var todos = [
+    "clean room",
+    "brush teeth",
+    "exercise",
+    "study javascript",
+    "eat healthy"
+];
+
+var todosImportant = [
+    "clean room!",
+    "brush teeth!",
+    "exercise!",
+    "study javascript!",
+    "eat healthy!"
+];
+
+var todosLength = todos.length;
+for (var i=0; i < todosLength; i++) {
+    console.log(i);
+}
+
+function logTodos(todo, i) {
+    console.log(todo, i);
+}
+
+todos.forEach(logTodos);
+todosImportant.forEach(logTodos);
+
+// todos.forEach(function(todos, j) {
+//     console.log(todos, j);
+// })
+
+// for (var i=0; i < todos.length; i++) {
+//     todos[i] = todos[i] + "!";
+// }
+
+// var counterOne = 0;
+// while (counterOne < 10) {
+//     console.log(counterOne);
+//     counterOne++
+// }
+
+// var counterTwo = 10
+// do {
+//     console.log(counterTwo);
+//     counterTwo--;
+// } while (counterTwo > 0);
