740262ea7dc932e6239d17fc7af42f4bb9280418
Olawale Ibrahim <ibrahim.olawale@earthanalytics.no>
2022-12-02 11:39:34 +0100

    functions

--- javascript/web-example/script.js
+++ javascript/web-example/script.js
@@ -1,4 +1,10 @@
-// alert("Helloo")
+function multiply(a, b) {
+    if (a > 10 || b > 10) {
+        return "this is too hard";
+    }
+    else {
+        return a * b;
+    }
+}
 
-console.log("Helloooo");
-console.log("Helloooo");
+alert(multiply(5, 6));
