cb36f3e747270a51ef1a994fca0d0ae54080e84a
Olawale Ibrahim <ibrahim.olawale@earthanalytics.no>
2022-12-02 12:34:00 +0100

    simple sign in page

--- javascript/web-example/script.js
+++ javascript/web-example/script.js
@@ -1,5 +1,31 @@
-var fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+var database = [
+    {
+        username: "olawale",
+        password: "supersecret"
+    }
+];
 
-fruits.shift();
-fruits.sort();
-fruits.reverse();
+var newsFeed = [
+    {
+        username: "Bobby",
+        timeline: "So tired from all that learning"
+    },
+    {
+        username: "Sally",
+        timeline: "Javascript is soo cool"
+    }
+];
+
+var userNamePrompt = prompt("What is your username?");
+var passwordPrompt = prompt("what is your password?");
+
+function signIn(user, pass) {
+    if (user == database[0].username 
+        && pass == database[0].password) {
+            console.log(newsFeed)
+        } else {
+            alert("Sorry, wrong username and password!");
+        }
+}
+
+signIn(userNamePrompt, passwordPrompt);
