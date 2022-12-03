7f2d90b744eef87e1848546b62bca03a6e3e1b64
Olawale Ibrahim <ibrahim.olawale@earthanalytics.no>
2022-12-03 16:46:21 +0100

    es5 and es6

---
+++ javascript/advanced-exercise/script.js
@@ -0,0 +1,69 @@
+function moveCommand(direction) {
+    var whatHappens;
+    switch (direction) {
+        case "forward":
+            whatHappens = "you encounter a monster";
+            break;
+        case "back":
+            whatHappens = "you arrived home";
+            break;
+        case "right":
+            whatHappens = "you found a river";
+            break;
+        case "left":
+            whatHappens = "you run into a troll";
+            break;  
+        default:
+            whatHappens = "please enter a valid direction"; 
+    }
+    return whatHappens;
+}
+
+// const player = "bobby";
+// let experience = 100;
+// let wizardLevel = false;
+
+if (experience > 90) {
+    let wizardLevel = true;
+}
+
+const obj = {
+    player: "bobby",
+    experience: 100,
+    wizardLevel: false,
+}
+
+const {player, experience} = obj;
+let { wizardLevel } = obj;
+
+// declaring object properties
+const obj1 = {
+    [username]: "Wale",
+    [1 + 2]: "hihi"
+}
+
+const a = "Simon";
+const b = true;
+const c = {};
+
+// const obj2 = {
+//     a: a,
+//     b: b,
+//     c: c
+// }
+
+const obj2 = {
+    a, b, c
+}
+
+// template strings
+const username = "Sally";
+const age = 34;
+const pet = "horse";
+
+const greetingBest = `Hello ${username} you seem to be ${age-10}. What a lovely ${pet} you have`;
+
+// symbol
+
+// arrow functions
+const add = (a, b) => a + b;
