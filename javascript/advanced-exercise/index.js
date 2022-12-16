016aa0b34f32657265990c7c2cffac5a5bf048db
Olawale Ibrahim <ibrahim.olawale13@gmail.com>
2022-12-16 11:12:01 +0100

    merge

--- javascript/advanced-exercise/index.js
+++ javascript/advanced-exercise/index.js
@@ -66,3 +66,24 @@ const getData = async function() {
         console.log("oooooopss", err);
     }
 };
+
+
+// object spread operator
+const animals = {
+    tiger: 23,
+    lion: 5,
+    monkey: 2
+}
+
+// const { tiger, ...rest } = animals;
+
+
+function objectSpread(p1, p2, p3) {
+    console.log(p1)
+    console.log(p2)
+    console.log(p3)
+}
+
+const { tiger, lion, ...rest } = animals;
+
+objectSpread(tiger, lion, rest);
